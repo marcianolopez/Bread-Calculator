bd2d0e2b6de431b1d0e9a8cdedfe2fff22d4eb3e
marcianolopez <91626739+marcianolopez@users.noreply.github.com>
2021-09-29 14:38:13 -0300

    Add files via upload
    Calculates quantities of water, yeast and salt for baking bread

---
+++ Formula Panadero.py
@@ -0,0 +1,76 @@
+from tkinter import *
+
+raiz= Tk()
+
+raiz.config(bg='purple', bd=60)
+
+miframe=Frame(raiz, width=1200, height=600, bg='light blue', bd=50)
+
+miframe.pack(expand=5)
+
+miharina=StringVar()
+miagua=StringVar()
+milevadura=StringVar()
+misal=StringVar()
+
+Titulo=Label(miframe, text='FORMULA PANADERO:', font=50, bg='light green' )
+Titulo.grid(row=0, column=0, sticky='W', padx=10, pady=10)
+
+cuadroharina=Entry(miframe, textvariable=miharina)
+cuadroharina.grid(row=1, column=1, padx=10, pady=10)
+
+
+cuadroagua=Entry(miframe, textvariable=miagua)
+cuadroagua.grid(row=2, column=1, padx=10, pady=10)
+
+cuadrolevadura=Entry(miframe, textvariable=milevadura)
+cuadrolevadura.grid(row=3, column=1, padx=10, pady=10)
+
+
+cuadrosal=Entry(miframe, textvariable=misal)
+cuadrosal.grid(row=4, column=1, padx=10, pady=10)
+
+def calculoagua():
+        
+        calculoagua=int(cuadroharina.get()) * (70) / (100) 
+
+        return miagua.set(calculoagua) 
+
+def calculolevadura():
+
+        calculolevadura=int(cuadroharina.get()) * (2) / (100)
+
+        return milevadura.set(calculolevadura)
+
+def calculosal():
+
+        calculosal=int(cuadroharina.get()) * (2) / (100)
+
+        return misal.set(calculosal)
+
+
+botoncalculo=Button(raiz, text='Realizar calculo', command=lambda: [calculoagua(), calculolevadura(), calculosal()])
+botoncalculo.pack()
+
+def cerrar():
+    raiz.destroy()
+
+botoncerrar=Button(raiz, text='Cerrar', command=cerrar)
+botoncerrar.pack(side=TOP)
+
+harinalabel=Label(miframe, text='Harina (en gramos):', font=30, bg='light green')
+harinalabel.grid(row=1, column=0, sticky='W', padx=10, pady=10)
+
+agualabel=Label(miframe, text='Agua ml:', font=30, bg='light green', textvariable=calculoagua)
+agualabel.grid(row=2, column=0, sticky='W', padx=10, pady=10)
+
+levaduralabel=Label(miframe, text='Levadura (en gramos):', font=30, bg='light green', textvariable=calculolevadura)
+levaduralabel.grid(row=3, column=0, sticky='W', padx=10, pady=10)
+
+sallabel=Label(miframe, text='Sal (en gramos):', font=30, bg='light green', textvariable=calculosal)
+sallabel.grid(row=4, column=0, sticky='W', padx=10, pady=10)
+
+
+
+
+raiz.mainloop()
